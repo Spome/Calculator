83cd6a01d575435fc2c97b78d989f3ce559f24f7
Spome <zaposar@gmail.com>
2022-02-02 15:14:47 +0200

    Cleaned up a lot of the code

--- script.js
+++ script.js
@@ -1,98 +1,40 @@
 
-let addButton = document.body.querySelector('#addButton');
-let subtractButton = document.body.querySelector('#subtractButton');
-let multiplyButton = document.body.querySelector('#multiplyButton');
-let divideButton = document.body.querySelector('#divideButton');
-
+let numberButtons = document.querySelectorAll(".numberButton");
 let operatorButtons = document.body.querySelectorAll(".operatorButton");
-
 let clearButton = document.body.querySelector('#clearButton');
-
 let operateButton = document.body.querySelector('#operateButton');
-
-let operatorType;
-let operatorToggle;
 
 let currentDisplay = document.body.querySelector('#currentDisplay');
 let historyDisplay = document.body.querySelector('#historyDisplay');
 
+let operatorType;
+let operatorToggle;
+
 let firstValue;
 let secondValue;
 
+numberButtons.forEach(function(element) {
+  element.addEventListener('click', addNumberToDisplay);
+});
 
-let inputValuesArray;
-
-let displayValue = "";
-
-function addOperatorToDisplay() {
-  [...operatorButtons].forEach(function(element) {
-    element.addEventListener('click', operatorFunction);
-  });
-}
-
-function operatorFunction() {
-
-  if (operatorType !== undefined) {
-    console.log("tes ttdst ")
-    computeAnswer();
-    operatorType = this.value;
-    firstValue = currentDisplay.textContent;
-    historyDisplay.textContent = `${firstValue} ${this.textContent}`
-    currentDisplay.textContent = "";
-    console.log(operatorType);
-    //operatorType = undefined;
-    //operatorFunction();
-    //operatorType = this.value;
-  }
-  else {
-    operatorType = this.value;
-    firstValue = currentDisplay.textContent;
-    historyDisplay.textContent = `${firstValue} ${this.textContent}`
-    currentDisplay.textContent = "";
-  }
-}
-
-
-function computeAnswer() {
-  if (operatorType !== undefined) {
-    secondValue = currentDisplay.textContent;
-    firstValue = +firstValue; // firstValue from string to number
-    secondValue = +secondValue; // secondValue from string to number
-    let finalAnswer;
-    switch(operatorType) {
-      case "add":
-        finalAnswer = operate(add, firstValue, secondValue);
-        break;
-      case "subtract":
-        finalAnswer = operate(subtract, firstValue, secondValue);
-        break;
-      case "multiply":
-        finalAnswer = operate(multiply, firstValue, secondValue);
-        break;
-      case "divide":
-        finalAnswer = operate(divide, firstValue, secondValue);
-    }
-    historyDisplay.textContent += ` ${currentDisplay.textContent}`
-    currentDisplay.textContent = finalAnswer;
-    operatorType = undefined;
-    }
-    // If operatorType is undefined, then do nothing.
-  
-  }
-
-
-operateButton.addEventListener("click", computeAnswer);
+operatorButtons.forEach(function(element) {
+  element.addEventListener('click', operatorFunction);
+});
 
 clearButton.addEventListener("click", clearInput);
 
+operateButton.addEventListener("click", computeAnswer);
 
 function addNumberToDisplay() {
-  let buttons = document.querySelectorAll(".numberButton");
-  [...buttons].forEach(function(element) {
-    element.addEventListener('click', function() {
-      currentDisplay.textContent += element.value;
-    });
-  });
+  currentDisplay.textContent += this.value;
+}
+
+function operatorFunction() {
+  if (operatorType !== undefined) computeAnswer();
+  operatorType = this.value;
+  firstValue = currentDisplay.textContent;
+  historyDisplay.textContent = `${firstValue} ${this.textContent}`
+  currentDisplay.textContent = "";
 }
 
 function clearInput() {
@@ -103,13 +45,49 @@ function clearInput() {
   operatorType = undefined;
 }
 
-function calculator() {
-  addNumberToDisplay();
-  addOperatorToDisplay();
-  computeAnswer();
-}
+function computeAnswer() {
+  if (operatorType !== undefined) {
+    secondValue = currentDisplay.textContent;
+    firstValue = +firstValue; // firstValue from string to number
+    secondValue = +secondValue; // secondValue from string to number
+    let answer;
+    let finalAnswer;
 
-calculator();
+    switch(operatorType) {
+      case "add":
+        answer = operate(add, firstValue, secondValue);
+        break;
+      case "subtract":
+        answer = operate(subtract, firstValue, secondValue);
+        break;
+      case "multiply":
+        answer = operate(multiply, firstValue, secondValue);
+        break;
+      case "divide":
+        answer = operate(divide, firstValue, secondValue);
+    }
+
+    historyDisplay.textContent += ` ${currentDisplay.textContent}`
+
+    if (countDecimals(answer) > 5) {
+      finalAnswer = answer.toFixed(5);
+      currentDisplay.textContent = finalAnswer
+    }
+    else {
+      currentDisplay.textContent = answer;
+    }
+
+    operatorType = undefined;
+    }
+
+    // If operatorType is undefined, then do nothing.
+  }
+
+function countDecimals(value) {
+  if (Math.floor(value) !== value) {
+    return value.toString().split(".")[1].length || 0;
+  }
+}
 
 function add(a,b) {
   return +a + +b;
